8b70693109b365d0a5c78db222c45b6d047dacf5
Hurntre <adefolajuariyo@gmail.com>
2019-12-19 01:13:28 +0100

    CH SIGNUP DOCUMENTATION
    - updated the swagger documentation to include that of the signup route
    - added password hash and token generator helper

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,7 +2,6 @@ import UserModel from '../db/models/user';
 
 const signUpController = (req, res) => {
   const { body } = req;
-
   UserModel.create(body, (error, data) => {
     if (error) {
       res.status(400).send({
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,5 +1,17 @@
 import mongoose from 'mongoose';
 import userSchema from '../schemas/userSchema';
+import authHelper from '../../helpers/auth';
+import tokenGenerator from '../../helpers/userToken';
+
+// eslint-disable-next-line func-names
+userSchema.pre('validate', async function() {
+  if (this.isNew) {
+    const hashedPassword = await authHelper.hashPassword(this.password);
+    this.password = hashedPassword;
+
+    this.token = tokenGenerator.userToken(this);
+  }
+});
 
 // USER MODEL
 const User = mongoose.model('User', userSchema);
